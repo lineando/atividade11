65d4602ba44178da4931b8c376b994336c2c4ae1
aline <165079083+lineando@users.noreply.github.com>
2024-09-24 19:00:23 +0000

    aline

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+// Exibe uma mensagem no console
 console.log('Olá mundo!');
-// Criando um novo elemento 
+
+// Criando um novo elemento <h1>
 let novoElemento = document.createElement('h1');
+
 // Alterando o conteúdo de texto do elemento
 novoElemento.innerText = 'Hello, World! English! (Inglês) Ok?!';
+
 // Selecionando o elemento body
 let elementoBody = document.body;
+
 // Colocando o novo elemento no body
 elementoBody.appendChild(novoElemento);
+
+// Estilizando o novo elemento
 novoElemento.style.backgroundColor = 'blue';
 novoElemento.style.color = 'yellow';
